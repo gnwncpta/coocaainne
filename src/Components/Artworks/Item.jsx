79e85ff89239e3176c03a1212238a5807667aa21
gnwncpta <gnwncpta@gmail.com>
2021-09-18 01:25:41 +0700

    change: design

--- src/Components/Artworks/Item.jsx
+++ src/Components/Artworks/Item.jsx
@@ -6,9 +6,10 @@ export default function Item(props){
     return (
         <div className="border border-white border-opacity-20">
             <img src={artwork} alt={artwork} width="330px" />
-            <div className="py-4">
+
+            <div className="py-5">
                 <h2 className="text-white px-7 text-xl font-medium">{name}</h2>
-                <p className="text-white px-7 text-sm text-gray-400">Created at 1 May 2020</p>
+                <p className="px-7 text-sm text-gray-400">Created at 1 May 2020</p>
             </div>
         </div>
     )
