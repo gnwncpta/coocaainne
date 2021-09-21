d21d268210a933fdce10db8a7804d7e562b98242
gnwncpta <gnwncpta@gmail.com>
2021-09-22 00:07:13 +0700

    customize: tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,43 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      inset: {
+        '1-prcnt': '1%',
+        '2-prcnt': '2%',
+        '3-prcnt': '3%',
+        '5-prcnt': '5%',
+        '10-prcnt': '10%',
+        '12-prcnt': '12%',
+        '15-prcnt': '15%',
+        '50-prcnt': '50%',
+        '20-screen': '20vw',
+        '25-screen': '25vw',
+        '30-screen': '30vw',
+        '33-screen': '33vw',
+        '40-screen': '40vw',
+        '50-screen': '50vw',
+      },
+      width: {
+        '100': '100px',
+        '200': '200px',
+        '300': '300px',
+        '400': '400px',
+        '500': '500px',
+        '600': '600px',
+        '700': '700px',
+        '800': '800px',
+        '900': '900px',
+        '1000': '1000px',
+      },
       height: {
+        '100': '100px',
+        '200': '200px',
+        '300': '300px',
+        '400': '400px',
+        '500': '500px',
+        '600': '600px',
+        '700': '700px',
+        '800': '800px',
         '900': '900px',
         '1000': '1000px',
       },
@@ -11,8 +47,19 @@ module.exports = {
         'cco': '#BAFF00',
         'darkcco': '#1f1f1f'
       },
+      screens: {
+        '1440': { 'max': '1440px' },
+        '1024': { 'max': '1024px' },
+        '768': { 'max': '768px' },
+        '320': { 'min': '320px' },
+        'mobile': { 'min': '320px', 'max': '480px' }
+      },
       zIndex: {
         '1': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
       }
     },
     fontFamily: {
