bdd874398ea3ba3e694290fb98000d6e6dbfd007
gnwncpta <gnwncpta@gmail.com>
2021-09-22 00:10:35 +0700

    update: SortMenu

--- src/Components/Artworks/SortMenu.jsx
+++ src/Components/Artworks/SortMenu.jsx
@@ -19,9 +19,9 @@ export default function SortMenu(props){
     }
 
     return (
-        <div className="bg-darkcco my-4 py-4 w-64 rounded-lg absolute">
-            <p className="py-4 px-6 text-white hover:bg-white hover:text-black cursor-pointer" onClick={oldestHandler}>Oldest</p>
-            <p className="py-4 px-6 text-white hover:bg-white hover:text-black cursor-pointer" onClick={newestHandler}>Newest</p>
+        <div className="bg-darkcco my-4 py-4 w-60 rounded-lg absolute">
+            <p className="py-4 px-6 text-white text-sm hover:bg-white hover:text-black cursor-pointer" onClick={oldestHandler}>Oldest</p>
+            <p className="py-4 px-6 text-white text-sm hover:bg-white hover:text-black cursor-pointer" onClick={newestHandler}>Newest</p>
         </div>
     )
 }
