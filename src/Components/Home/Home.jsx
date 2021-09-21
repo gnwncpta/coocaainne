1e305ef458b50f46dfb938b818383f45a9ca8160
gnwncpta <gnwncpta@gmail.com>
2021-09-22 00:11:29 +0700

    update: Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,27 +1,41 @@
+import { useEffect } from 'react';
 import 'tailwindcss/tailwind.css';
+import './Home.css';
 import me from '../../assets/Image.svg';
 
 export default function Home(props){
-    return (
-        <div className="bg-black mx-auto h-900 flex items-center">
-            <div className="w-full mx-32 flex items-center justify-between"> 
 
-                <div className="typography mx-20 relative">
+    const { show, setShow } = props;
+
+    useEffect(() => {
+        window.document.title = "Home";
+        return () => window.document.title = "COOCAAINNE";
+    });
+
+    function menuHandler(){
+        if(show) setShow(false);
+    }
+
+    return (
+        <div className="bg-black mx-auto h-max lg:h-900 flex items-center justify-start md:h-max" onClick={menuHandler}>
+            <div className="w-full mx-8 my-8 lg:my-12 flex flex-col items-center justify-between lg:flex-row 2xl:mx-32">
+                <div className="typography w-full mb-22 relative lg:mx-10 2xl:mx-20">
                     <div className="z-10 relative">
-                        <h1 className="text-white text-7xl font-light mb-7 leading-tight"><span className="text-black">W</span>elcome to<br />Koken Crib</h1>
-                        <p className="text-white mb-7 font-light text-sm">What's good man ? Take a time to explore my crib.<br />Are you a dev ? </p>
+                        <h1 className="text-white font-light mb-7 leading-normal text-5xl 2xl:text-7xl lg:text-6xl">
+                            <span className="text-black">W</span>elcome to<br />Koken's Crib
+                        </h1>
+                        <p className="text-white mb-7 font-light text-sm">What's good man ? Take a time to explore my crib.<br />Are you a dev ?</p>
 
                         <div className="buttons">
-                            <button className="py-2 px-10 rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white">Explore</button>
-                            <button className="py-2 px-10 text-white">Download My VC</button>
+                            <a href="http://localhost:3000/artworks" className="py-2 px-8 rounded-full bg-white text-sm">Explore</a>
+                            <button className="py-2 px-8 text-white text-sm">Download CV</button>
                         </div>
                     </div>
 
-                    <div className="rounded-full w-24 h-24 bg-cco absolute -top-1.5 -left-4"></div>
+                    <div className="rounded-full w-20 h-20 bg-cco absolute -top-2 -left-4 lg:w-20 lg:h-20 lg:-top-3 lg:-left-3 md:w-20 md:h-20"></div>
                 </div>
 
-                <img alt="me" src={me} className="mx-20" />
-
+                <img alt="me" src={me} className="w-max mx-22 my-20 2xl:w-max lg:w-500 md:w-400" />
             </div>
         </div>
     )
