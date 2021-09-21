cd7b8add59fb963cee5ac7ef2842fa3057391821
gnwncpta <gnwncpta@gmail.com>
2021-09-22 05:41:18 +0700

    update: design

--- src/Components/Design/Design.jsx
+++ src/Components/Design/Design.jsx
@@ -10,9 +10,13 @@ export default function Design(props){
     });
 
     return (
-        <div className="bg-black h-screen">
-            <div className="h-900 flex items-center justify-center mx-32">
-                <h1 className="text-white text-lg">Page on Working!</h1>
+        <div className="bg-darkcco h-screen">
+            <div className="h-400 flex flex-col items-center justify-center mx-32">
+                <h1 className="text-white text-md flex flex-col items-center">
+                    <p>On Working!</p> <br />
+                    <span className="font-mono font-medium text-black bg-white px-4 py-2 rounded-md">Design</span>
+                </h1>
+                <p className="mt-10 text-sm font-ligth text-white text-center">I need to research the design for this page. <br />You see this because there is no design to show.</p>
             </div>
         </div>
     )
