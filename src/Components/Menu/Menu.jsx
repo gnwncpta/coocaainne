15bf496a1396dadc6ee3d4fc8fe42168374baa72
gnwncpta <gnwncpta@gmail.com>
2021-09-22 00:11:41 +0700

    update: Menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,47 +1,77 @@
+import { useState } from 'react';
 import "tailwindcss/tailwind.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 // Components
 import Home from '../Home/Home';
 import Artworks from '../Artworks/Artworks';
+import ArchivedDesign from '../ArchivedDesign/ArchivedDesign';
+import Design from '../Design/Design';
 import About from '../About/About';
 
 // Assets
 import logo from '../../assets/cco-logo.png';
+import house_icon from '../../assets/house.svg';
+import x from '../../assets/X.svg';
 
 export default function Menu(){
+
+    const [ show, setShow ] = useState(false);
+
+    function menuHandler(){
+        if(show) setShow(false);
+    }
+
     return (
         <Router>
-            <section className="mx-auto bg-black py-10">
-                <header className="mx-32 text-white flex items-center justify-between">
+            <section className="mx-auto bg-black py-10" onClick={menuHandler}>
+                <header className="2xl:mx-32 text-white flex items-center justify-between mx-8">
                     <div className="logo">
                         <Link to="/">
                             <img src={logo} width="40px" className="cursor-pointer" alt="Logo" />
                         </Link>
                     </div>
 
-                    <div>
-                        <Link to="/artworks" className="py-2 px-6 mx-5 text-sm rounded-full hover:bg-white hover:text-black">Artworks</Link>
-                        <Link to="/archived-design" className="py-2 px-6 mx-5 text-sm rounded-full hover:bg-white hover:text-black">Archived Design</Link>
-                        <Link to="/design" className="py-2 px-6 mx-5 text-sm rounded-full hover:bg-white hover:text-black">Design</Link>
+                    <div className="hidden lg:flex flex-col lg:static lg:flex-row lg:bg-transparent lg:py-0">
+
+                        <Link to="/artworks" className="py-2 px-6 my-2 mx-5 text-sm rounded-full text-black hover:bg-white hover:text-black lg:text-white">Artworks</Link>
+                        <Link to="/archived-design" className="py-2 px-6 my-2 mx-5 text-sm rounded-full text-black hover:bg-white hover:text-black lg:text-white">Archived Design</Link>
+                        <Link to="/design" className="py-2 px-6 my-2 mx-5 text-sm rounded-full text-black hover:bg-white hover:text-black lg:text-white">Design</Link>
+
                     </div>
 
-                    <Link to="/about" className="py-2 px-8 text-sm rounded-full hover:bg-cco hover:text-black">About</Link>
+                    <div className="z-20 w-screen py-2 fixed bottom-1-prcnt left-0 flex flex-col items-center justify-center">
+                        {show &&
+                            <div className="z-20 mb-3 right-20-screen bottom-12-prcnt bg-cco py-8 flex flex-col rounded-lg lg:static lg:flex-row lg:bg-transparent lg:py-0">
+
+                                <Link to="/artworks" className="py-2 px-6 my-2 mx-5 text-sm rounded-full text-black hover:bg-white hover:text-black">Artworks</Link>
+                                <Link to="/archived-design" className="py-2 px-6 my-2 mx-5 text-sm rounded-full text-black hover:bg-white hover:text-black">Archived Design</Link>
+                                <Link to="/design" className="py-2 px-6 my-2 mx-5 text-sm rounded-full text-black hover:bg-white hover:text-black">Design</Link>
+
+                            </div>
+                        }
+
+                        <div className="px-5 py-5 shadow-2xl rounded-full bg-cco focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white cursor-pointer lg:hidden" onClick={() => setShow(show ? false : true)}>
+                            { show ? <img src={x} width="24px" alt="cross_icon" /> : <img src={house_icon} width="24px" alt="house_icon" /> }
+                        </div>
+                    </div>
+
+                    <Link to="/about" className="py-2 px-8 text-sm rounded-full hover:bg-cco hover:text-black">Profile</Link>
                 </header>
             </section>
 
             <Switch>
                 <Route exact path="/">
-                    <Home />
+                    <Home show={show} setShow={setShow} />
                 </Route>
                 <Route path="/artworks">
-                    <Artworks />
+                    <Artworks show={show} setShow={setShow} />
                 </Route>
                 <Route path="/archived-design">
-                    {/* <ArchivedDesign /> */}
+                    <ArchivedDesign />
                 </Route>
                 <Route path="/design">
-                    {/* <Design /> */}
+                    <Design />
                 </Route>
                 <Route path="/about">
                     <About />
