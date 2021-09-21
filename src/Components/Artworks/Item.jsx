fc77e7398840ece7a571d6b58f4eafecffcb1d9a
gnwncpta <gnwncpta@gmail.com>
2021-09-22 05:47:48 +0700

    update: item

--- src/Components/Artworks/Item.jsx
+++ src/Components/Artworks/Item.jsx
@@ -8,15 +8,10 @@ export default function Item(props){
         <div className="border border-white border-opacity-20">
             <img src={artwork} alt={artwork} width="330px" />
 
-            <div className="w-10/12	mx-auto py-5 border-b border-white border-opacity-20">
+            <div className="w-10/12	mx-auto py-5 border-opacity-20">
                 <h2 className="text-white text-xl font-medium">{name}</h2>
                 <p className="text-sm text-gray-400">Created at {date}</p>
             </div>
-
-            <button className="group flex items-center mx-7 my-2 py-2 text-sm text-gray-400 hover:text-white">
-                <p className="">View Artwork</p>
-                <img src={arrow} alt="arrow" width="22px" className="ml-4 hidden group-hover:inline-block"/>
-            </button>
         </div>
     )
 }
