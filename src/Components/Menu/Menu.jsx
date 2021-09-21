9076ef8d8126d9c9d9584a607f5bb162d287cb4b
gnwncpta <gnwncpta@gmail.com>
2021-09-22 03:19:42 +0700

    change: navigation margin

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -34,9 +34,9 @@ export default function Menu(){
                     </div>
 
                     <div className="hidden lg:flex flex-col lg:static lg:flex-row lg:bg-transparent lg:py-0">
-                        <Link to="/artworks" className="py-2 px-6 my-2 mx-5 text-sm rounded-full text-black hover:bg-white hover:text-black lg:text-white">Artworks</Link>
-                        <Link to="/archived-design" className="py-2 px-6 my-2 mx-5 text-sm rounded-full text-black hover:bg-white hover:text-black lg:text-white">Archived Design</Link>
-                        <Link to="/design" className="py-2 px-6 my-2 mx-5 text-sm rounded-full text-black hover:bg-white hover:text-black lg:text-white">Design</Link>
+                        <Link to="/artworks" className="py-2 px-6 my-2 mx-5 lg:mx-2 text-sm rounded-full text-black hover:bg-white hover:text-black lg:text-white">Artworks</Link>
+                        <Link to="/archived-design" className="py-2 px-6 my-2 mx-5 lg:mx-2 text-sm rounded-full text-black hover:bg-white hover:text-black lg:text-white">Archived Design</Link>
+                        <Link to="/design" className="py-2 px-6 my-2 mx-5 lg:mx-2 text-sm rounded-full text-black hover:bg-white hover:text-black lg:text-white">Design</Link>
                     </div>
 
                     <div className="z-20 w-screen py-2 fixed bottom-1-prcnt left-0 flex flex-col items-center justify-center">
